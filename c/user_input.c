49e6abc1581d1399c77c214ddf799559529767d5
vishal shinde <vishalkshinde3735@gmail.com>
2025-07-30 14:26:18 +0530

    30 july 2025

---
+++ c/user_input.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,k;
+    float b,n;
+    char c;
+    double d;
+    printf("Enter the value of a and k: ");
+    scanf("%d%d",&a,&b);
+    printf("Enter the value of b and n: ");
+    scanf("%f%f",&c,&n);
+     printf("Enter the value of c: ");
+    scanf("%c",&c);
+     printf("Enter the value of d: ");
+    scanf("%lf",&d);
+    // printf("a=%d k=%d b=%f n=%f c=%c d=%lf",a,k,b,n,c,d);
+    return 0;
+    
+}
---
+++ datatype.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    char name[] ="vishal shinde";
+    int age[] = 21;
+    char email[] = "vishal@gmail.com";
+
+    printf( "name: %c ,name");
+
+    return 0;
+}
